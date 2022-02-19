7ac3fd064833792cb5b4463794c50ff256ad31f3
3vilBonobo <iboutsika@gmail.com>
2022-02-19 21:01:07 +0200

    add roll btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Die from './components/Die';
 
 const App = () => {
+  let randomDieValues = [];
+  for (let i = 0; i < 10; i++) {
+    randomDieValues.push(Math.floor(Math.random() * 6 + 1));
+  }
+
+  const dieElements = randomDieValues.map((value, idx) => (
+    <Die value={value} key={idx} />
+  ));
+  const rollDice = () => {
+    console.log('rolled');
+  };
   return (
     <main className="App">
       <div className="game-container">
-        <Die value={'test'} />
+        {dieElements}
+        <button className="btn" type="button" onClick={rollDice}>
+          {'Roll'}
+        </button>
       </div>
     </main>
   );
